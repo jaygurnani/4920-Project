7bb6e8fde8a90f210791e8c5d9ad4a86f3d4d9f1
Jay Gurnani <jay.gurnani@gmail.com>
2012-10-09 15:11:30 +1100

    Added Command interface

---
+++ src/servlets/Controller.java
@@ -0,0 +1,47 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String command = request.getParameter("command");
+		
+		if(command.equals("register"))	{
+			//FORWARD TO REGISTER
+			getServletContext().getRequestDispatcher("/Register").forward(request, response);
+		} else {
+			//NO SPECIFIC COMMAND, SEND BACK TO INDEX.
+			response.sendRedirect("index.jsp");
+			return;
+		}
+		
+	}
+
+}
